0da0e4ef042acf6b1f2b80090eac1bcd5a76c549
daejo <daejo@icloud.com>
2020-08-20 21:42:07 -0700

    fixes time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,48 @@
-var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17]; //Timeblocks from 9a.m to 5p.m.
 
-var currentHour = moment().hour();
+var currentHour = moment().hour(); //Actual time.
 
-function displayHour() {
-    for (let index = 0; index < workHours.length; index++) {
+setInterval(() => { //Day
+    var nowReadable = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');;
+    currentDay.textContent = nowReadable;
+},1000);
+
+function displayHour() { //Creates boxes
+    for (let i = 0; i < workHours.length; i++) { //Standard for loop.
 
         var row = $("<div class= 'row'>"); // dynamic HTML
         var col1 = $("<div class = 'col-sm-2 text-right'>"); //move the text to the right label
 
-        var getWorkHour = workHours[index] + " A.M."
+        var getWorkHour = workHours[i] + " P.M." //Adds p.m. to 12
 
-        if (workHours[index] <= 12) {
-            getworkHour = workHours[index] + " P.M"
-            
-            if (workHours[index] >=13) {
-                getworkHour = workHours[index] -12 + " P.M." //convert to 1pm etc
+            if (workHours[i] <= 11) { //Adds a.m. and p.m. accordingly.
+                getWorkHour = workHours[i] + " A.M"               
+            } else if (workHours[i] > 12) { //Converts 12+ numbers to p.m.
+                getWorkHour = workHours[i] -12 + " P.M." 
             }
-        }
-        col1.append(getWorkHour);
 
-        var col2 = $("<div class =col-sm-8>");
+        col1.append(getWorkHour); //Creates time of day column.
 
-        var textarea = $("<textarea class =form-control>");
-        textarea.attr("id", "textarea" + index);
+        var col2 = $("<div class ='col-sm-8'>"); //Creates task textbox column.
+
+        var textarea = $("<textarea class ='form-control bg-warning text-black'>");
+        textarea.attr("id", "textarea" + i);
        
 
-        if(currentHour > workHours[index]) {
+        if(currentHour > workHours[i]) {
             textarea.addClass("bg-secondary");
         }
 
-
-
-        if(currentHour === workHours[index]) {
+        if(currentHour === workHours[i]) {
             textarea.addClass("bg-danger");
         }
 
-
-
-        if(currentHour < workHours[index]) {
+        if(currentHour < workHours[i]) {
             textarea.addClass("bg-success");
         }
 
         //local storage
-        var getText = localStorage.getItem("textarea" + index);
+        var getText = localStorage.getItem("textarea" + i);
 
         textarea.text(getText);
 
