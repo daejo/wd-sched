e7a499b400788aeebe0ccdd14ec66d376fdeede6
daejo <daejo@icloud.com>
2020-08-20 21:00:13 -0700

    fixes framework

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,75 @@
-/* PSEUDOCODE
-    Planner Displays current day, date, and time.
-    Input Tasks for respective time of day
-    Save task
-    As current time moves closer to set, time blocks change colors.
-*/
+var workHours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-setInterval(() => {
-    var nowReadable = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');;
-    currentDay.textContent = nowReadable;
-},1000);
+var currentHour = moment().hour();
 
+function displayHour() {
+    for (let index = 0; index < workHours.length; index++) {
+
+        var row = $("<div class= 'row'>"); // dynamic HTML
+        var col1 = $("<div class = 'col-sm-2 text-right'>"); //move the text to the right label
+
+        var getWorkHour = workHours[index] + " A.M."
+
+        if (workHours[index] <= 12) {
+            getworkHour = workHours[index] + " P.M"
+            
+            if (workHours[index] >=13) {
+                getworkHour = workHours[index] -12 + " P.M." //convert to 1pm etc
+            }
+        }
+        col1.append(getWorkHour);
+
+        var col2 = $("<div class =col-sm-8>");
+
+        var textarea = $("<textarea class =form-control>");
+        textarea.attr("id", "textarea" + index);
+       
+
+        if(currentHour > workHours[index]) {
+            textarea.addClass("bg-secondary");
+        }
+
+
+
+        if(currentHour === workHours[index]) {
+            textarea.addClass("bg-danger");
+        }
+
+
+
+        if(currentHour < workHours[index]) {
+            textarea.addClass("bg-success");
+        }
+
+        //local storage
+        var getText = localStorage.getItem("textarea" + index);
+
+        textarea.text(getText);
+
+        col2.append(textarea)
+
+        var col3 = $("<div class = col'col-sm-2>");
+
+        var button = $("<button>");
+
+        button.addClass("saveButton");
+
+        button.text("Save");
+
+        col3.append(button);
+
+
+
+
+        row.append(col1, col2, col3)
+        $("#planner").append(row)
+    }
+}
+
+
+
+displayHour();
+
+$(".saveButton").on("click", function() {
+
+})
